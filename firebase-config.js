2f20bdff100ac1513922b406b6917c2b9a4d8768
bhavishyamishra1210-a11y <bhavishyamishra1210@gmail.com>
2025-11-22 00:24:51 +0530

    Refactor firebase-config.js for clarity and structure

--- firebase-config.js
+++ firebase-config.js
@@ -1,36 +1,19 @@
-// firebase-config.js
-// Replace the values inside firebaseConfig with your project's values
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  sendPasswordResetEmail,
-  signOut,
-  updateProfile,
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  query,
-  orderBy,
-  getDocs,
-  where
-} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "REPLACE_API_KEY",
-  authDomain: "REPLACE_AUTH_DOMAIN",
-  projectId: "REPLACE_PROJECT_ID",
-  storageBucket: "REPLACE_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_MESSAGING_SENDER_ID",
-  appId: "REPLACE_APP_ID"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, signOut, updateProfile, onAuthStateChanged, collection, addDoc, query, orderBy, getDocs, where };
+[001] // firebase-config.js
+[002] import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+[003] import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, signOut, updateProfile, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+[004] import { getFirestore, collection, addDoc, query, orderBy, getDocs, where } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+[005]
+[006] const firebaseConfig = {
+[007]   apiKey: "REPLACE_API_KEY",
+[008]   authDomain: "REPLACE_AUTH_DOMAIN",
+[009]   projectId: "REPLACE_PROJECT_ID",
+[010]   storageBucket: "REPLACE_STORAGE_BUCKET",
+[011]   messagingSenderId: "REPLACE_MESSAGING_SENDER_ID",
+[012]   appId: "REPLACE_APP_ID"
+[013] };
+[014]
+[015] const app = initializeApp(firebaseConfig);
+[016] const auth = getAuth(app);
+[017] const db = getFirestore(app);
+[018]
+[019] export { auth, db, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, signOut, updateProfile, onAuthStateChanged, collection, addDoc, query, orderBy, getDocs, where };
